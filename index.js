b8ed98eb777d41f43ec315a8f171a7d822803443
Sonia <serrano_sonia@hotmail.com>
2021-04-05 09:33:01 -0400

    For reviewing

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Your code here
+
+function saturdayFun(activity = "roller-skate"){
+    return (`This Saturday, I want to ${activity}!`);
+}
+
+mondayWork = function(activity="go to the office"){
+    return (`This Monday, I will ${activity}.`);
+}
+
+function wrapAdjective(caracEsp="*"){
+    return function(def="special"){
+        return `You are ${caracEsp}${def}${caracEsp}!`;
+    }
+}
+
+const Calculator = {
+    add: function(number1,number2){
+        return number1 + number2;
+    },
+    subtract: function(number1,number2){
+        return number1 - number2;
+    },
+    multiply: function(number1,number2){
+        return number1 * number2;
+    },
+    divide: function(number1,number2){
+        return number1 / number2;
+    }
+}
+
+console.log(typeof(Calculator));
+
+function actionApplyer(par1,arrayOfTransforms=[]){
+    if (arrayOfTransforms.length===0){
+        return par1;
+    }
+    else{
+        arrayOfTransforms = [
+            function(par1){ return par1 * 2 },
+            function(par1){ return par1 + 1000},
+            function(par1){ return par1 % 7 }
+        ];
+        return 4;
+    }
+}
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
